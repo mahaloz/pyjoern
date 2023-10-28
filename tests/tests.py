aa93a7330211c88ae76686d7ffb9807642c81bb6
Zion Leonahenahe Basque <zion@zionbasque.com>
2023-10-28 16:50:03 -0700

    Add server tests (#3)
    * Add server tests
    
    * bump

--- tests/tests.py
+++ tests/tests.py
@@ -2,15 +2,27 @@ import sys
 import unittest
 from pathlib import Path
 
-from pyjoern import fast_cfgs_from_source
+from pyjoern import fast_cfgs_from_source, JoernClient, JoernServer
 
 TEST_SOURCE_DIR = Path(__file__).absolute().parent / "source"
 
 
-class TestCFGLifting(unittest.TestCase):
-    def test_cfg_structure(self):
+class TestFastLifters(unittest.TestCase):
+    def test_fast_cfg(self):
         cfg = fast_cfgs_from_source(TEST_SOURCE_DIR / "simple.c")["main"]
         assert cfg is not None
+
+
+class TestJoernServer(unittest.TestCase):
+    def test_server(self):
+        with JoernServer() as server:
+            assert server is not None
+
+            client = JoernClient(TEST_SOURCE_DIR / "simple.c", port=server.port)
+            assert client is not None
+
+            funcs = client.functions
+            assert 'main' in funcs
 
 
 if __name__ == "__main__":
